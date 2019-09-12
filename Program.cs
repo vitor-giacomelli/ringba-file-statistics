28f938c54d990b477360dab55c2397d44b0f91da
Vitor Giacomelli <kruxer1985@hotmail.com>
2019-09-12 11:14:37 -0300

    Implement word and prefix count

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 
 namespace ringba_test
 {
@@ -18,6 +21,7 @@ namespace ringba_test
             int[] chars = new int[char.MaxValue];
             int countUppercase = 0;
             string fileString = File.ReadAllText(fileName);
+            string[] splitText = Regex.Split(fileString, @"(?<!^)(?=[A-Z])");
 
 
             foreach (char t in fileString)
@@ -37,9 +41,39 @@ namespace ringba_test
                     Console.WriteLine("Letter: {0}  Frequency: {1}", (char)i, chars[i]);
                 }
             }
+
+            var prefixCount = GetPrefixCount(splitText);
+
+            var wordCount = GetWordCount(splitText);
+
+
             Console.WriteLine("Number of capitalized letters: {0}", countUppercase);
+
+            Console.WriteLine("Most common word: {0}  Frequency: {1}", wordCount.FirstOrDefault().Key, wordCount.FirstOrDefault().Value);
+            Console.WriteLine("Most common prefix: {0}  Frequency: {1}", prefixCount.FirstOrDefault().Key, prefixCount.FirstOrDefault().Value);
+
 
             Console.ReadLine();
         }
+
+        private static IOrderedEnumerable<KeyValuePair<string, int>> GetWordCount(string[] list)
+        {
+            return list
+                .SelectMany(x => x.Split())
+                .GroupBy(x => x)
+                .Select(x => new KeyValuePair<string, int>(x.Key, x.Count()))
+                .OrderByDescending(x => x.Value);
+        }
+
+        private static IOrderedEnumerable<KeyValuePair<string, int>> GetPrefixCount(string[] list)
+        {
+            return list
+               .Where(x => x.Length > 1)
+               .SelectMany(x => x.Substring(0, 2).Split())
+               .GroupBy(x => x)
+               .Select(x => new KeyValuePair<string, int>(x.Key, x.Count()))
+               .OrderByDescending(x => x.Value);
+        }
+
     }
 }
