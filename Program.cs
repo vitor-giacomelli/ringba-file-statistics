8891ab86e5532fe01692e2a4bb53ec760c434932
Vitor Giacomelli <kruxer1985@hotmail.com>
2019-09-12 09:52:27 -0300

    Implement download of the file, count of each letter and capitalized ones

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 
 namespace ringba_test
 {
@@ -6,7 +8,38 @@ namespace ringba_test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string remoteUri = "https://ringba-test-html.s3-us-west-1.amazonaws.com/TestQuestions/output.txt";
+            string fileName = Directory.GetCurrentDirectory() + "file.txt";
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile(remoteUri, fileName);
+
+
+            int[] chars = new int[char.MaxValue];
+            int countUppercase = 0;
+            string fileString = File.ReadAllText(fileName);
+
+
+            foreach (char t in fileString)
+            {
+                //Counting the characters frequency
+                chars[(int)t]++;
+
+                //Counting the capitalized letters
+                if (char.IsUpper(t))
+                    countUppercase++;
+            }
+
+            for (int i = 0; i < (int)char.MaxValue; i++)
+            {
+                if (chars[i] > 0 && char.IsLetterOrDigit((char)i))
+                {
+                    Console.WriteLine("Letter: {0}  Frequency: {1}", (char)i, chars[i]);
+                }
+            }
+            Console.WriteLine("Number of capitalized letters: {0}", countUppercase);
+
+            Console.ReadLine();
         }
     }
 }
